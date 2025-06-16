f0d83e0148299fbb2cd4d616656b4a21493b0a56
mfoniso <mfonisoischris@gmail.com>
2025-06-16 23:28:50 +0100

    fixes: email template fixes

--- resources/views/emails/invoice/pending.blade.php
+++ resources/views/emails/invoice/pending.blade.php
@@ -1,56 +1,29 @@
 @extends('layouts.emails.user')
-@section('head')
-    <style>
 
-        .invoice-table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .invoice-table th, .invoice-table td {
-            border: 1px solid #ddd;
-            padding: 8px;
-        }
-
-        .invoice-table th {
-            background-color: #f2f2f2;
-            text-align: left;
-        }
-
-        .invoice-table td {
-            text-align: right;
-        }
-
-        .invoice-table td.description {
-            text-align: left;
-        }
-
-        .invoice-total {
-            font-weight: bold;
-        }
-    </style>
-@endsection
 @section('content')
-
     <div>
-        <p>Hi {{$invoice->user->name}},</p>
         <p>
-            This is a friendly reminder that you have items waiting for you in your {{config('app.name')}} cart!
+            Hi {{$invoice->user->name}},
         </p>
         <p>
-            We've saved the following items for you:
+            This is a friendly reminder that you have items waiting for you in your Outside wears cart!
         </p>
+        <p>We've saved the following items for you:</p>
+        <x-emails.order-button :invoice="$invoice" />
     </div>
 
-    <x-emails.invoice-table :invoice="$invoice"/>
+    <x-emails.order-details :invoice="$invoice" />
+    <x-emails.billing-info :invoice="$invoice" />
+
     <div>
-        <h5>
-            Important Note:
-        </h5>
-        <p>This is not a confirmed order and the items in your cart are not reserved until you complete the checkout
-            process.
-            <br>
-            We look forward to processing your order soon!
+        <p>Questions?</p>
+        <p>
+            If you have any questions about your order or its delivery, please don't hesitate to contact our customer
+            support team at <a href="mailto:support@wearoutsider.com">support@wearoutsider.com</a>.
+        </p>
+        <p>
+            We're here to help! <br>
+            We look forward to getting your order to you soon!
         </p>
     </div>
 
