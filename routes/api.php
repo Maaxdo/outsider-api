4908128d14c2cd385f3a05c70d3dc96b5e1315c4
mfoniso <mfonisoischris@gmail.com>
2025-06-17 13:12:45 +0100

    fixes: removed unnecessary imports

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-use Intervention\Image\ImageManager;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
